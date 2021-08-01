c148697814800c604a4be4c14d8fb1001b2dba50
nagi-moriyama-lvgs <nagi.moriyama@leverages.jp>
2021-08-01 14:28:19 +0900

    readmeの作成

---
+++ README.md
@@ -0,0 +1,13 @@
+#必要パッケージのinstall
+
+```
+$ npm init
+$ npm install node-fetch jsdom fs --save-dev
+```
+
+#スクレイピングの実行
+
+```
+$ ./index.mjs
+```
+上記を実行することで同じディレクトリにout.txtというファイルが作成される。
--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import fetch from "node-fetch";
 import jsdom from "jsdom";
 import fs from "fs";
@@ -9,6 +8,7 @@ const { JSDOM } = jsdom;
 let csvData = "";
 let url =
   "https://job.rikunabi.com/2022/search/company/result/?fw=&ms=0&kk=0&wk=2&wk=3&wk=4&wk=5&aot=&apr=&cwr=&cmr=&rry=3&rr=&ggr=1&gr=&awy=&wer=&wmr=&ad=7&ad=11&ad=12&exr=1&hu=1";
+
 const scraping = async (url) => {
   const res = await fetch(url);
   const html = await res.text();
@@ -30,7 +30,6 @@ const scraping = async (url) => {
   const nextUrl =
     "https://job.rikunabi.com/" + nextPageButton.getAttribute("href");
 
-  console.log(nextUrl);
   fs.writeFile("out.txt", csvData, (err) => {
     if (err) console.log(err);
     else console.log("write end");
